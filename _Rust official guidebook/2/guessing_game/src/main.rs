39c2b498cf9f9338c80382d400dee5e8d93f1c0a
Ben <ben399399@naver.com>
2020-12-19 23:28:40 +0900

    :sparkles: guessing game: Added random number

--- _Rust official guidebook/2/guessing_game/src/main.rs
+++ _Rust official guidebook/2/guessing_game/src/main.rs
@@ -1,19 +1,37 @@
-use std::io;//* load io library
+use rand::Rng;
 use std::any::type_name;
+use std::cmp::Ordering;
+use std::io; //* load io library
+///* use random library
 
 fn type_of<T>(_: T) -> &'static str {
-    type_name::<T>()
+  type_name::<T>()
 }
 
-fn main(){//* declare & implement main function
-    println!("let the game begin!");
-    println!("write the number you guessed");
+fn main() {
+  //* declare & implement main function
+  println!("let the game begin!");
+  println!("write the number you guessed");
+  let secret_number = rand::thread_rng().gen_range(1, 101); //* use random seed from OS
+                                                            //* values from [)
+  let mut guess = String::new(); //* create mutable variable
 
-    let mut guess = String::new();//* create mutable variable 
+  io::stdin()
+    .read_line(&mut guess) //* Returns Result type
+    //* Result is Union type of
+    //* | Ok
+    //* | Err
+    .expect("failed to read value");
+  //* program will run without line above, but will show an error
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read value");
+  let guess: i32 = guess.trim().parse().expect("failed to read value");
+  //* shadowing is possible in rust, override name 'guess from here
 
-    println!("input value {} {}",guess,type_of(&guess));
+  match guess.cmp(&secret_number) {
+    Ordering::Less => println!("input value is smaller than it thought"),
+    Ordering::Greater => println!("input value is larger than it thought"),
+    Ordering::Equal => println!("exactly!"),
+  }
+
+  println!("input value {} {}", guess, type_of(&guess));
 }
