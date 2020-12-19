bbe65ee256c96245701fc033e7af2573185f9d80
Ben <ben399399@naver.com>
2020-12-20 00:02:04 +0900

    :tada: gussing game: Finished

--- _Rust official guidebook/2/guessing_game/src/main.rs
+++ _Rust official guidebook/2/guessing_game/src/main.rs
@@ -13,25 +13,31 @@ fn main() {
   println!("let the game begin!");
   println!("write the number you guessed");
   let secret_number = rand::thread_rng().gen_range(1, 101); //* use random seed from OS
-                                                            //* values from [)
-  let mut guess = String::new(); //* create mutable variable
+  println!("answer is {}", secret_number);
+  loop {
+    let mut guess = String::new(); //* create mutable variable
+                                   //* values from [)
+    io::stdin()
+      .read_line(&mut guess) //* Returns Result type
+      //* Result is Union type of
+      //* | Ok
+      //* | Err
+      .expect("failed to read value");
+    //* program will run without line above, but will show an error
 
-  io::stdin()
-    .read_line(&mut guess) //* Returns Result type
-    //* Result is Union type of
-    //* | Ok
-    //* | Err
-    .expect("failed to read value");
-  //* program will run without line above, but will show an error
-
-  let guess: i32 = guess.trim().parse().expect("failed to read value");
-  //* shadowing is possible in rust, override name 'guess from here
-
-  match guess.cmp(&secret_number) {
-    Ordering::Less => println!("input value is smaller than it thought"),
-    Ordering::Greater => println!("input value is larger than it thought"),
-    Ordering::Equal => println!("exactly!"),
+    let guess: i32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
+    //* shadowing is possible in rust, override name 'guess from here
+    println!("input value {} {}", guess, type_of(&guess));
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("input value is smaller than it thought"),
+      Ordering::Greater => println!("input value is larger than it thought"),
+      Ordering::Equal => {
+        println!("exactly!");
+        break;
+      }
+    }
   }
-
-  println!("input value {} {}", guess, type_of(&guess));
 }
